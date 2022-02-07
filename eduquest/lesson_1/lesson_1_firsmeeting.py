3398e30de87cb25a7e3dba01b74e00949e34b44f
Nlegion <nparanoid@gmail.com>
2022-02-07 05:37:37 +0300

    070222

---
+++ eduquest/lesson_1/lesson_1_firsmeeting.py
@@ -0,0 +1,14 @@
+import story
+
+# Урок 1. Знакомство
+# Изучаемый материал print, input, переменная, текст, строка, f строка
+
+print("Привет! Добро пожаловать в образовательный квест!")
+print("Перед тем как мы начнем, Представься!")
+
+hero_name = input('введите имя героя:')
+hero_race = input('к какому народу герой принадлежит?')
+hero_class = input('профессия героя?')
+hero_age = int(input('сколько герою лет?'))
+
+print(story.story_text(hero_name, hero_class, hero_race, hero_age))
