2d34b0825615730d807ce59518711e90e0aae332
Nlegion <nparanoid@gmail.com>
2022-02-27 14:45:01 +0300

    270222
    270222

--- ya_lic/2_3_slise/11.py
+++
@@ -1,3 +0,0 @@
-full_name = 'Иванов И. И.'
-surname = full_name[:-6]
-print(surname)
--- ya_lic/2_3_slise/4.py
+++ ya_lic/2_3_slise/4.py
@@ -1,5 +1,14 @@
 # Формат телефонного номера в США обычно такой: сначала идет +1, затем 10 цифр, разделенных скобками и дефисами по 3 или 4:
 # +1(XXX)XXX-XXXX
 
-st = input()
-print(f'+1({st[0:3:]}){st[3:6:]}-{st[6::]}')
+a, b = input(), int(input())
+
+while True:
+    if len(a) != 0:
+        print(a[-b:])
+        a = a[:(len(a) - b if len(a) - b > 0 else 0)]
+    if len(a) != 0:
+        print(a[:b])
+        a = a[b:]
+    if len(a) == 0:
+        break
