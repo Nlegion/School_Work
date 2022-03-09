9ab5fa6af79d176b15f031fcd876e2f1ea564c46
Nlegion <nparanoid@gmail.com>
2022-03-09 15:49:13 +0300

    90322
    90322

---
+++ 6class/test1.py
@@ -0,0 +1,25 @@
+
+print("линейный:", 'три ' * 10)
+
+
+
+
+#
+# for item in range(10):
+#     print("цикл 1:", 'три')
+# x = 0
+# while x != 10:
+#     print("цикл 2:",'три')
+#     x += 1
+#
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
+# print('три')
