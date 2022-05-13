35d757762019ff00577f0d7933ea81089a5b6263
Nlegion <nparanoid@gmail.com>
2022-05-13 15:22:27 +0300

    smth
    smth

---
+++ 1.py
@@ -0,0 +1,8 @@
+i = 1
+while i < 10:
+    j = 1
+    while j < 10:
+        a = i * j
+        print(a)
+        j = j + 1
+    i = i + 1
--- 6class/111.py
+++ 6class/111.py
@@ -1,11 +1,3 @@
-x = int(input())
-z = 0
-y = 0
-while x > 0:
-    z = x % 10
-    x = x // 10
-    if z <= y:
-        print('no')
-        break
-    else:
-        print('yes')
+for x in range(1, 9):
+    for y in range(1, 9):
+        print(x * y)
